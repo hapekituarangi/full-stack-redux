72db290f3b263d3b1ce2a80997d60e24dfc89dad
hapekituarangi <hape.porter@gmail.com>
2016-07-04 14:20:08 +1200

    improved basic immutable test

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -19,25 +19,31 @@ describe('immutability', () => {
 
   });
 
-  describe('A List', () => {
+  describe('a tree', () => {
 
       function addMovie(currentState, movie) {
-        return currentState.push(movie);
+        return currentState.update('movies', movies => movies.push(movie));
       }
 
       it('is immutable', () => {
-        let state = List.of('Trainspotting', '28 Days Later');
+        let state = Map({
+          movies: List.of('Trainspotting', '28 Days Later')
+        });
         let nextState = addMovie(state, 'Sunshine');
 
-        expect(nextState).to.equal(List.of(
-          'Trainspotting',
-          '28 Days Later',
-          'Sunshine'
-        ));
-        expect(state).to.equal(List.of(
-          'Trainspotting',
-          '28 Days Later'
-        ));
+        expect(nextState).to.equal(Map({
+          movies: List.of(
+            'Trainspotting',
+            '28 Days Later',
+            'Sunshine'
+          )
+        }));
+        expect(state).to.equal(Map({
+          movies: List.of(
+            'Trainspotting',
+            '28 Days Later'
+          )
+        }));
       });
 
     });
